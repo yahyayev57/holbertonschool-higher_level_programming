2a0bacd7703afe58a4878051f3ace8a40a51ab64
Kenan <yahyayevkenan57@gmail.com>
2025-09-20 21:02:05 +0400

    .

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str1 = "object-oriented"
 str2 = " programming"
-str3 = " with"
-str4 = " Python"
-print(str1 + str2 + str3 + str4)
+str3 = " with Python"
+print(str1 + str2 + str3)
