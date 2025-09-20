f369aaf118bdcd7eb88335a39d66fe74ba8ac1c3
Kenan <yahyayevkenan57@gmail.com>
2025-09-20 21:08:15 +0400

    .

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-str1 = "object-oriented"
-str2 = " programming"
-str3 = " with"
-str4 = " Python"
-print(str1 + str2 + str3 + str4)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[0:6]
+print(str)
