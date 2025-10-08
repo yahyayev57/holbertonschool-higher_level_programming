48d3017ef2766357e35469e3ecf3f83f74b75fba
Kenan <yahyayevkenan57@gmail.com>
2025-10-08 22:59:29 +0400

    Update 7-insert_value.sql

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+-- Inserts a new row in the table `first_table`
+-- in database `hbtn_0c_0` in MySQL Server.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School")
